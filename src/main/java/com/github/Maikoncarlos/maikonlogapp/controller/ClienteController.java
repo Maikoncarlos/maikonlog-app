013543e3ca0949e214fcc70e38ddf400ae372013
Maikoncarlos <maikonsaturno@gmail.com>
2022-05-19 12:34:28 -0300

    implementing content Negotiation to xml

--- src/main/java/com/github/Maikoncarlos/maikonlogapp/controller/ClienteController.java
+++ src/main/java/com/github/Maikoncarlos/maikonlogapp/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.github.Maikoncarlos.maikonlogapp.controller;
 
+
 import com.github.Maikoncarlos.maikonlogapp.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1")
 public class ClienteController {
 
     @GetMapping("/clientes")
