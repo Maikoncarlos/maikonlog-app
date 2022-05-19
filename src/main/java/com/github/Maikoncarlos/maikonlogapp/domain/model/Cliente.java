89f885b6741e0142c2c2786d60249575f163dad5
Maikoncarlos <maikonsaturno@gmail.com>
2022-05-19 20:20:07 -0300

    implementing jpa

--- src/main/java/com/github/Maikoncarlos/maikonlogapp/domain/model/Cliente.java
+++ src/main/java/com/github/Maikoncarlos/maikonlogapp/domain/model/Cliente.java
@@ -1,13 +1,25 @@
 package com.github.Maikoncarlos.maikonlogapp.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter @Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100)
     private String nome;
+    @Column(length = 100)
     private String email;
+    @Column(length = 20)
     private String telefone ;
 }
