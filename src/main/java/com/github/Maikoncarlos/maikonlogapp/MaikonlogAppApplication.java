c0860c794f1d95473ff7125388bf7fb02a9ee02d
Maikoncarlos <maikonsaturno@gmail.com>
2022-05-19 10:33:33 -0300

    first commit

---
+++ src/main/java/com/github/Maikoncarlos/maikonlogapp/MaikonlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.Maikoncarlos.maikonlogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaikonlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaikonlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Maikoncarlos/maikonlogapp/MaikonlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Maikoncarlos.maikonlogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaikonlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
